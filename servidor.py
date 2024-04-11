1e6fe9fd287af7c67d7a08f0a630d05cf76c4c26
nacho <nacho.boero@gmail.com>
2024-04-11 12:56:32 -0300

    1

--- servidor.py
+++ servidor.py
@@ -148,4 +148,4 @@ if __name__ == '__main__':
     load_dotenv()
     port = int(os.getenv('PORT', 8000))
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=port)
+    uvicorn.run(app,host="0.0.0.0", port=port)
